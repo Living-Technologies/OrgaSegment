8ca31ad00cc4abef71682e71f646564c245e94db
svbeuningen <11160433+svbeuningen@users.noreply.github.com>
2021-06-22 15:31:19 +0200

    change softmax

--- lib/model.py
+++ lib/model.py
@@ -2,11 +2,6 @@ from keras.layers import Input, Conv2D, Conv2DTranspose, BatchNormalization, Act
 from keras.models import Model
 from keras.optimizers import *
 
-# function that defines one convolutional layer with certain number of filters
-def _single_conv(input_tensor, n_filters, kernel_size):
-    x = Conv2D(filters = n_filters, kernel_size = (kernel_size, kernel_size), activation = 'sigmoid')(input_tensor)
-    return x
-
 # function that defines two sequential 2D convolutional layers with certain number of filters
 def _double_conv(input_tensor, n_filters, kernel_size = 3):
     x = Conv2D(filters = n_filters, kernel_size = (kernel_size, kernel_size), padding = 'same', kernel_initializer = 'he_normal')(input_tensor)
@@ -31,6 +26,11 @@ def _pooling(input_tensor, dropout_rate = 0.1):
 # function that merges two layers (Concatenate)
 def _merge(input1, input2):
     x = concatenate([input1, input2])
+    return x
+
+# function that defines one convolutional layer with certain number of filters
+def _output(input_tensor, n_filters, kernel_size):
+    x = Conv2D(filters = n_filters, kernel_size = (kernel_size, kernel_size), activation = 'softmax')(input_tensor)
     return x
 
 def UNet(input_size, n_filters):
@@ -78,7 +78,7 @@ def UNet(input_size, n_filters):
     conv9 = _double_conv(up9, n_filters * 1)
 
     # output layer
-    output = _single_conv(conv9, 1, 1)
+    output = _output(conv9, 1, 1)
 
     # model
     model = Model(inputs = input, outputs = output)
