dd4ac088033f26c2218ab9fe41ea13a5e13b828c
svbeuningen <11160433+svbeuningen@users.noreply.github.com>
2021-06-30 13:26:49 +0200

    added color_mode

--- lib/io.py
+++ lib/io.py
@@ -193,7 +193,7 @@ class OrganoidDataset(utils.Dataset):
         specs in image_info.
         """
         info = self.image_info[image_id]
-        img = load_img(info['path'], target_size=(info['height'], info['width']), color_mode=color_mode)
+        img = load_img(info['path'], target_size=(info['height'], info['width']), color_mode=info['color_mode'])
         if img_bit_depth:
             img = np.asarray(img) / ((2 ** info['img_bit_depth']))
         else:
