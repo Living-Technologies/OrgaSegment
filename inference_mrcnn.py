46a75b83d4e7e4669a7ab14d270afc1aeacfe1c9
svbeuningen <11160433+svbeuningen@users.noreply.github.com>
2021-07-12 14:28:15 +0200

    improve results

--- inference_mrcnn.py
+++ inference_mrcnn.py
@@ -18,6 +18,7 @@ import shutil
 from skimage.io import imsave
 import pandas as pd
 import numpy as np
+import random
 
 #Set Tensorflow logging
 logger.info(f'Tensorflow version: {tf.__version__}')
@@ -64,7 +65,7 @@ def main():
     model.load_weights(config.MODEL, by_name=True)
 
 
-    bbox =  pd.DataFrame({'image': pd.Series([], dtype='str'),
+    results =  pd.DataFrame({'image': pd.Series([], dtype='str'),
                           'mask': pd.Series([], dtype='str'),
                           'name': pd.Series([], dtype='str'),
                           'y1':  pd.Series([], dtype=np.int16),
@@ -83,37 +84,41 @@ def main():
                                i, 
                                use_mini_mask=False)
         
-        #Create mask file
+        #Create mask
         len_masks = gt_mask.shape[-1]
         values = list(range(1, len_masks + 1)) #Create a list of values from 1 to number of masks per label
-                
+        mask_name = f'{config.INFERENCE_DIR}{data.info(i)["id"]}_orgaseg_masks.png'
+
+        #Process masks
         for count, i in enumerate(range(len_masks)):
+            #Get mask information
             msk = gt_mask[:,:,i].astype(int)
+            size = np.sum(msk)
             num = values.pop(random.randrange(len(values))) #Get a random number for mask
             msk = np.where(msk != 0, num, msk)
             if count == 0:
                 mask = msk
             else:
                 mask = np.maximum(mask, msk) #Combine previous mask with new mask
+
+            #Set all information
+            info = {'image': data.image_info(i)['path'],
+                    'mask': mask_name,
+                    'name': data.info(i)['id'],
+                    'mask_id': num
+                    'y1': gt_bbox[i,0],
+                    'x1': gt_bbox[i,1],
+                    'y2': gt_bbox[i,2],
+                    'x2': gt_bbox[i,3],
+                    'class': gt_class_id[i],
+                    'size': size}
+            results = results.append(info, ignore_index=True)
         
-        mask_name = f'{config.INFERENCE_DIR}{data.info(i)["id"]}_orgaseg_masks.png'
-        #Save mask file
+        #Save mask
         imsave(mask_name)
 
-        #Append bbox and class information
-        for i in range(gt_bbox.shape[0]):
-            bb = {'image': data.image_info(i)['path'],
-                  'mask': mask_name,
-                  'name': data.info(i)['id'],
-                  'y1': gt_bbox[i,0],
-                  'x1': gt_bbox[i,1],
-                  'y2': gt_bbox[i,2],
-                  'x2': gt_bbox[i,3],
-                  'class': gt_class_id[i]}
-            bbox = bbox.append(bb, ignore_index=True)
-    
-    #Save bbox information
-    bbox.to_csv(f'{config.INFERENCE_DIR}orgaseg_bbox.csv', ignore_index=True)
+    #Save results
+    results.to_csv(f'{config.INFERENCE_DIR}orgaseg_results.csv', ignore_index=True)
         
 if __name__ == "__main__":
     logger.info('Start inference...')
