4591cc0e7a4218ad1541fe3c6af70623a3729312
svbeuningen <11160433+svbeuningen@users.noreply.github.com>
2021-07-13 13:49:17 +0200

    add tracking

---
+++ track.py
@@ -0,0 +1,58 @@
+#Init logger
+import logging
+import logging.config
+logging.config.fileConfig('conf/logging.conf')
+logger = logging.getLogger(__file__)
+
+#Import packages
+import sys
+import shutil
+import pandas as pd
+import trackpy as tp
+import re
+import os
+
+#Get Job ID
+job_id=sys.argv[1]
+
+#Data folder
+data_dir=sys.argv[2]
+if os.path.isdir(data_dir) == False:
+            logger.error(f'Incorrect data path specified: {data_dir}')
+            exit(1)
+else:
+    data_dir=os.path.join(data_dir, '')
+    logger.info(f'Data dir: {data_dir}')
+
+#Regex
+regex=sys.argv[3]
+
+def main():
+    #Get data
+    logger.info('Get Orgasegment Results')
+    results = pd.read_csv(f'{data_dir}orgaseg_results.csv')
+
+    #Enrich data
+    results['well'] = re.search(regex, results['name']).group('WELL')
+    results['t'] = re.search(regex, results['name']).group('T')
+
+    ## Calculate centers and track organoids over time
+    logger.info('Start tracking organoids')
+
+    results['x'] = (results['x2'] + results['x1']) / 2
+    results['y'] = (results['y2'] + results['y1']) / 2
+    results = results.groupby('well').apply(tp.link, search_range=20, memory=2, t_column='t').reset_index(drop=True)
+    
+    ## Filter out particles that are seen less than 50% of the time
+    results = results[results.groupby(['well','particle'])['t'].transform('count') >= (max(results['t']) / 2)]
+    
+    #Save results
+    results.to_csv(f'{data_dir}orgaseg_tracked.csv', index=False)
+        
+if __name__ == "__main__":
+    logger.info('Start tracking job...')
+    main()
+    logger.info('Tracking job completed!')
+    ##Copy logging to data dir
+    shutil.copy(f'log/JobName.{job_id}.out', f'{data_dir}/JobName.{job_id}.out')
+    shutil.copy(f'log/JobName.{job_id}.err', f'{data_dir}/JobName.{job_id}.err')
---
+++ track.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --partition=cpu
+#SBATCH --nodes=1
+#SBATCH --cpus-per-node=4
+#SBATCH --time=2:00:00
+#SBATCH --mem=32G
+#SBATCH --mail-user=s.vanbeuningen@umcutrecht.nl
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+#SBATCH --error=log/JobName.%J.err
+#SBATCH --output=log/JobName.%J.out
+
+ENV=OrgaSegment
+DATA_DIR='/hpc/umc_beekman/DIS/20210526/DIS_COPY'
+REGEX='DIS_(?P<WELL>[A-Z][0-9]+) P.*_t(?P<T>[0-9]+)_ch.*'
+
+source ~/.bashrc
+
+cd $SLURM_SUBMIT_DIR
+
+echo $ENV
+
+conda activate $ENV
+
+conda info --envs
+
+nvidia-smi
+
+## execute python script
+python track.py $SLURM_JOB_ID $DATA_DIR $REGEX
+
+conda deactivate
