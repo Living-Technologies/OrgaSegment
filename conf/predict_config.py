ea87b38f68b236dec2cd799e984d448b92668b9d
svbeuningen <11160433+svbeuningen@users.noreply.github.com>
2021-08-11 10:41:39 +0200

    set max dim for very large images

--- conf/predict_config.py
+++ conf/predict_config.py
@@ -13,7 +13,7 @@ class PredictConfig(TrainConfig):
 
     # Input image resizing
     IMAGE_RESIZE_MODE = 'pad64'
-    IMAGE_MAX_DIM = 2240 #To prevent OOM on GPU
+    IMAGE_MAX_DIM = 2048 #To prevent OOM on GPU
 
     # OrgaSegment specific config
     MODEL_DIR = '/hpc/umc_beekman/orgasegment/models/'
