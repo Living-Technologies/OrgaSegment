f62711b0c0f63850ce084d889fabca187d6cdd2e
svbeuningen <11160433+svbeuningen@users.noreply.github.com>
2022-03-23 16:36:47 +0100

    fixed neptune metrics

--- eval_mrcnn.py
+++ eval_mrcnn.py
@@ -151,24 +151,25 @@ def main():
                         'fn': fn[t]}
                 evaluation = evaluation.append(info, ignore_index=True)
 
-                run[f'eval/ID_{i}/image'] = data_eval.info(i)['path']
+                run[f'eval/images/ID_{i}'] = data_eval.info(i)['path']
                 #AP
                 run[f'eval/class_name={class_name}/ID_{i}/ap'].log(value=ap[t], step=round(config.AP_THRESHOLDS[t], 2))
-                run[f'eval/ID_{i}/class_name={class_name}/ID_{i}/ap/{round(config.AP_THRESHOLDS[t], 2)}'] = ap[t]
+                run[f'eval/class_name={class_name}/ID_{i}/ap/{round(config.AP_THRESHOLDS[t], 2)}'] = ap[t]
                 #TP
                 run[f'eval/class_name={class_name}/ID_{i}/tp'].log(value=tp[t], step=round(config.AP_THRESHOLDS[t], 2))
-                run[f'eval/ID_{i}/class_name={class_name}/ID_{i}/tp/{round(config.AP_THRESHOLDS[t], 2)}'] = tp[t]
+                run[f'eval/class_name={class_name}/ID_{i}/tp/{round(config.AP_THRESHOLDS[t], 2)}'] = tp[t]
                 #FP
                 run[f'eval/class_name={class_name}/ID_{i}/fp'].log(value=fp[t], step=round(config.AP_THRESHOLDS[t], 2))
-                run[f'eval/ID_{i}/class_name={class_name}/ID_{i}/fp/{round(config.AP_THRESHOLDS[t], 2)}'] = fp[t]
+                run[f'eval/class_name={class_name}/ID_{i}/fp/{round(config.AP_THRESHOLDS[t], 2)}'] = fp[t]
                 #FN
                 run[f'eval/class_name={class_name}/ID_{i}/fn'].log(value=fn[t], step=round(config.AP_THRESHOLDS[t], 2))
-                run[f'eval/ID_{i}/class_name={class_name}/ID_{i}/fn/{round(config.AP_THRESHOLDS[t], 2)}'] = fn[t]
+                run[f'eval/class_name={class_name}/ID_{i}/fn/{round(config.AP_THRESHOLDS[t], 2)}'] = fn[t]
 
  
     summary = evaluation.groupby(['class_name', 'threshold'], as_index=False)['ap'].mean()
     for i in range(len(summary)):
-        neptune.log_metric(name=f'eval/class_name={summary["class_name"][i]}/mAP', x=summary['threshold'][i], y=ap[i])
+        run[f'eval/class_name={summary["class_name"][i]}/mAP'].log(value=summary['ap'][i], step=summary['threshold'][i])
+        run[f'eval/class_name={summary["class_name"][i]}/mAP/{summary["threshold"][i]}'] = summary['ap'][i]
 
     #Save results
     evaluation.to_csv(model_name.replace('.h5', '_evaluation.csv'), index=False)
