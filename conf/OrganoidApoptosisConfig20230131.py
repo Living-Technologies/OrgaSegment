d0c39dcf6542f97c81f03f978a9d303624524f9d
Sam van Beuningen <s.vanbeuningen@umcutrecht.nl>
2023-02-01 10:43:57 +0100

    added correct model to config

--- conf/OrganoidApoptosisConfig20230131.py
+++ conf/OrganoidApoptosisConfig20230131.py
@@ -129,6 +129,6 @@ class PredictConfig(TrainConfig):
 
     # OrgaSegment specific config
     MODEL_DIR = '/hpc/umc_beekman/orgasegment/models/'
-    MODEL_NAME = 'organoid-apoptosis20220314T1417'
+    MODEL_NAME = 'organoid-apoptosis20230131T2039'
     MODEL = '/hpc/umc_beekman/orgasegment/models/' + MODEL_NAME + '/mask_rcnn_organoid-apoptosis_0500.h5'
     COLOR_MODE = 'grayscale'
